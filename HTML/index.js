60acdd2a04e42fa1900cbd82c2c0d7207c114cee
obliviator123 <80073355+obliviator123@users.noreply.github.com>
2023-10-26 22:19:32 +0100

    first commit

---
+++ HTML/index.js
@@ -0,0 +1,20 @@
+let x = 10;
+
+
+function date() {
+    document.getElementById('demo').innerHTML = Date()
+}
+
+
+function warning() {
+    alert("Warning!");
+}
+
+function dark() {
+    document.body.style.backgroundColor = "#0a171a";
+}
+
+function light() {
+    document.body.style.backgroundColor = "#aaaaaa";
+}
+
